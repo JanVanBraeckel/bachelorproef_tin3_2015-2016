f928982b8a89bacf3eb49a0a3563f963c795d760
Jan Van Braeckel <janvanbraeckel1995@hotmail.com>
2016-05-24 19:56:32 +0200

    Add GNU license to arduino code

--- Scriptie/source/HeartrateSwitch.ino
+++ Scriptie/source/HeartrateSwitch.ino
@@ -1,4 +1,20 @@
 /*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
  * Little project based on https://www.arduino.cc/en/Tutorial/Genuino101CurieBLEHeartRateMonitor
  * I've added a custom service to enable a switch when sending anything other than 0, and disabling it by sending 0
  */
